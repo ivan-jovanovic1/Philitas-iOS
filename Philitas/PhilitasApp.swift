088433b7298473a98d915f6fcecc3804aafa0a90
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-03-27 18:21:35 +0200

    Initial Commit

---
+++ Philitas/PhilitasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhilitasApp.swift
+//  Philitas
+//
+//  Created by Ivan Jovanović on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhilitasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
