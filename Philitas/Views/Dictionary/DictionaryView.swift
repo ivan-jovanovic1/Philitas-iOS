442f36e0ebdb6ac6101672ec4e543fd310448643
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-04-04 00:19:01 +0200

    Update to reflect changes in store

--- Philitas/Views/Dictionary/DictionaryView.swift
+++ Philitas/Views/Dictionary/DictionaryView.swift
@@ -15,52 +15,70 @@ struct DictionaryView: View {
 	}
 
 	var body: some View {
+
 		NavigationView {
-			List(store.words) { word in
-                NavigationLink(destination: WordDetailsView(wordId: word._id)) {
-					WordRow(
-						word: word.word,
-						language: word.language,
-						translated: ""
-					)
-				}
-
-				if store.shouldShowNextPage(word: word) {
-					ProgressView()
-						.task(store.loadWords)
-					//                        .onAppear(perform: model.loadWords)
-				}
-			}
-
-			.searchable(
-				text: $store.searchString,
-				placement: .navigationBarDrawer(displayMode: .always),
-				prompt: "Iskanje"
-			) {
-				//                ForEach(model.searchWords) { word in
-				//                        WordRow(
-				//                            word: word.word,
-				//                            language: word.language,
-				//                            translated: word.translation
-				//                        )
-				//                }
-			}
-			.navigationTitle("Slovar")
+            switch store.words {
+            case .loading:
+                ProgressView()
+            case .error(let error):
+                Text(error.localizedDescription)
+            case .data(let data):
+                wordsList(data)
+            }
 		}
+		.navigationViewStyle(.stack)
 		.background(.red)
-		.task(store.loadWords)
-		.refreshable(action: store.loadWords)
-		.onReceive(store.$searchString.debounce(for: 0.5, scheduler: DispatchQueue.main)) { _ in
-			store.searchForWords()
-		}
+		.task { await store.loadWords() }
+//		.onReceive(store.$searchString.debounce(for: 0.5, scheduler: DispatchQueue.main)) { _ in
+//
+//		}
 	}
+    
+    @ViewBuilder
+    func wordsList(_ data: [DictionaryStore.ViewModel]) -> some View {
+        List(data) { word in
+            NavigationLink(destination: WordDetailsView(wordId: word._id)) {
+                WordRow(
+                    word: word.word,
+                    language: word.language,
+                    translated: ""
+                )
+            }
+
+            if store.shouldShowNextPage(word: word) {
+                ProgressView()
+                    .task { await store.loadWords() }
+            }
+        }
+        .refreshable { await store.loadWords(refreshing: true) }
+        .searchable(
+            text: $store.searchString,
+            placement: .navigationBarDrawer(displayMode: .always),
+            prompt: "Iskanje"
+        ) {
+            switch store.wordFromSearch {
+            case .loading:
+                ProgressView()
+            case .error(let error):
+                Text(error.localizedDescription)
+            case .data(let data):
+                Text(data.word)                
+            case .none:
+                EmptyView()
+            }
+        }
+        .onSubmit(of: .search) {
+            Task { await store.searchForWord() }
+        }
+        .navigationTitle("Slovar")
+    }
 }
 
 #if DEBUG
-struct DictionaryView_Previews: PreviewProvider {
-	static var previews: some View {
-		DictionaryView()
-			.previewDevice("iPhone 13 Pro")
+	struct DictionaryView_Previews: PreviewProvider {
+		static var previews: some View {
+			DictionaryView()
+				.previewDevice("iPhone 13 Pro")
+		}
 	}
-}
 #endif
