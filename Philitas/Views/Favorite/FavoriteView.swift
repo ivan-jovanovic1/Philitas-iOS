6f385d5258974e61600d790febee274456c7c32b
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-06-11 00:26:22 +0200

    Format code.

--- Philitas/Views/Favorite/FavoriteView.swift
+++ Philitas/Views/Favorite/FavoriteView.swift
@@ -9,64 +9,60 @@ import SwiftUI
 
 
 struct FavoriteView<T: FavoriteLoader>: View {
-    
-    @Environment(\.isSearching) var isSearching
-    @StateObject private var store: FavoriteStore<T>
-
-    init(
-        service: T
-    ) {
-        _store = StateObject(wrappedValue: FavoriteStore(service: service))
+  
+  @EnvironmentObject private var session: Session
+  @StateObject private var store: FavoriteStore<T>
+  
+  init(
+    service: T,
+    selectedTab: Binding<TabItem>
+  ) {
+    _store = StateObject(wrappedValue: FavoriteStore(service: service))
+  }
+  
+  var body: some View {
+    NavigationView {
+      switch store.favorites {
+      case .loading:
+        ProgressView()
+      case let .error(error):
+        Text(error.localizedDescription)
+      case let .data(data):
+        wordList(data)
+      }
     }
-    
-    var body: some View {
-        NavigationView {
-            switch store.favorites {
-            case .loading:
-                ProgressView()
-            case let .error(error):
-                Text(error.localizedDescription)
-            case let .data(data):
-                wordList(data)
-            }
-        }
-        .navigationViewStyle(.stack)
-        .background(.red)
-        .task { await store.loadFavorites() }
-//        .onChange(of: isSearching) {
-//            if !$0 {
-//                store.resetSearchState()
-//            }
-//        }
+    .navigationViewStyle(.stack)
+    .background(.red)
+    .task { await store.loadFavorites() }
+  }
+  
+  @ViewBuilder
+  func wordList(_ data: [T.Item]) -> some View {
+    List(data) { word in
+      NavigationLink(
+        destination: WordDetailsView(loader: WordDetailsService(wordId: word._id))
+      ) {
+        WordRow(
+          word: word.word,
+          language: word.language,
+          translated: ""
+        )
+      }
+      if store.shouldShowNextPage(word: word) {
+        ProgressView()
+          .task { await store.loadFavorites() }
+      }
     }
-    
-    @ViewBuilder
-    func wordList(_ data: [T.Item]) -> some View {
-        List(data) { word in
-            NavigationLink(
-                destination: WordDetailsView(loader: WordDetailsService(wordId: word._id))
-            ) {
-                WordRow(
-                    word: word.word,
-                    language: word.language,
-                    translated: ""
-                )
-            }
-            if store.shouldShowNextPage(word: word) {
-                ProgressView()
-                    .task { await store.loadFavorites() }
-            }
-        }
-        .refreshable { await store.loadFavorites(refreshing: true) }
-//        .searchable(
-//            text: $store.searchString,
-//            placement: .navigationBarDrawer(displayMode: .always),
-//            prompt: "Iskanje",
-//            suggestions: searchView
-//        )
-//        .onSubmit(of: .search) {
-//            //            Task { await store.searchForWord() }
-//        }
-        .navigationTitle("Priljubljeno")
-    }
+    .refreshable { await store.loadFavorites(refreshing: true) }
+    //        .searchable(
+    //            text: $store.searchString,
+    //            placement: .navigationBarDrawer(displayMode: .always),
+    //            prompt: "Iskanje",
+    //            suggestions: searchView
+    //        )
+    //        .onSubmit(of: .search) {
+    //            //            Task { await store.searchForWord() }
+    //        }
+    .navigationTitle("Priljubljeno")
+  }
 }
