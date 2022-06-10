efbfc7c0331b825a2659fd8ce4a783a05eb910e8
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-06-10 23:50:41 +0200

    Implement SessionUpdater in Service.

--- Philitas/App/SessionService.swift
+++ Philitas/App/SessionService.swift
@@ -11,8 +11,14 @@ class SessionService: SessionLoader {
     func loadFromToken() async throws -> SessionLoader.User {
         try await UserAPI.userFromToken().data
     }
+}
 
+extension SessionService: SessionUpdater {
     func login(username: String, password: String) async throws -> SessionLoader.User {
         try await UserAPI.login(username: username, password: password).data
+    }
+    
+    func logout() async throws -> Bool {
+        try await UserAPI.logout().data        
     }
 }
