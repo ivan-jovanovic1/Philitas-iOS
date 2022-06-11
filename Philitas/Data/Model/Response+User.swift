dbd747d71c3ffd98cd8df57f8e326b122cb1d085
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-06-11 21:36:34 +0200

    Rename jwsToken to authToken.

--- Philitas/Data/Model/Response+User.swift
+++ Philitas/Data/Model/Response+User.swift
@@ -9,10 +9,12 @@ import Foundation
 
 public extension Response {
     struct UserData: Decodable, Equatable {
+        private(set) var id: String
         private(set) var username: String
         private(set) var email: String
-        private(set) var jwsToken: String
+        private(set) var authToken: String
         private(set) var firstName: String?
         private(set) var lastName: String?
+        private(set) var favoriteWordIds: [String]
     }
 }
