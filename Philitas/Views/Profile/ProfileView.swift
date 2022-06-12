66f04e5f9376f2788edc5020c13cbcde60466b02
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-06-12 18:55:53 +0200

    Move sections to separate functions.

--- Philitas/Views/Profile/ProfileView.swift
+++ Philitas/Views/Profile/ProfileView.swift
@@ -29,78 +29,12 @@ struct ProfileView: View {
             store.checkForFullName()
         }
     }
-    
-    private func mainView(user: SessionLoader.User) -> some View {
-        VStack {
-            Image(systemName: "person.crop.circle")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80, height: 80)
-            
-            Form {
-                Section {
-                    HStack {
-                        Text("Uporabniško ime")
-                        Spacer()
-                        Text(user.username)
-                    }
-                    
-                    if let fullName = store.fullName {
-                        HStack {
-                            Text("Ime in priimek")
-                            Spacer()
-                            Text(fullName)
-                        }
-                    }
-                } header: {
-                    header(imageName: "person", description: "Ime")
-                }
-                
-                Section {
-                    Text(user.email)
-                } header: {
-                    header(imageName: "envelope", description: "E-naslov")
-                }
-                
-                Section {
-                    HStack {
-                        Text("Število")
-                        Spacer()
-                        Text("\(user.favoriteWordIds.count)")
-                    }
-                } header: {
-                    header(imageName: "star", description: "Priljubljene besede")
-                }
-                
-                Section {
-                    AsyncButton(role: .destructive) {
-                        await session.logout()
-                    } label: {
-                        HStack {
-                            Text("Odjava")
-                        }
-                    }
-                } header: {
-                    Image(systemName: "rectangle.portrait.and.arrow.right")
-                        .font(.title2)
-                        .foregroundColor(.red)
-                }
-            }
-        }
-    }
-    
-    private func header(imageName: String, description: String) -> some View {
-        HStack {
-            Text("\(Image(systemName: imageName))")
-                .font(.title2)
-            
-            Text(description)
-                .font(.body)
-        }
-    }
-    
+}
+
+// MARK: - Login or Register
+private extension ProfileView {
     @ViewBuilder
-    private var loginOrRegister: some View {
+    var loginOrRegister: some View {
         Text("""
         \(Image(systemName: "info.circle")) Za ogled profila se morate prijaviti.
         V primeru da še nimate računa, Vam je na voljo registracija.
@@ -132,6 +66,91 @@ struct ProfileView: View {
         .padding(.horizontal, 16)
         
         Spacer()
+    }
+}
+
+// MARK: - Main viewx
+private extension ProfileView {
+    func mainView(user: SessionLoader.User) -> some View {
+        VStack {
+            Image(systemName: "person.crop.circle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 80, height: 80)
+            
+            Form {
+                section(username: user.username, fullName: store.fullName)
+                section(email: user.email)
+                section(favorites: user.favoriteWordIds.count)
+                logout {
+                    await session.logout()
+                }
+            }
+        }
+    }
+    
+    func section(username: String, fullName: String?) -> some View {
+        Section {
+            HStack {
+                Text("Uporabniško ime")
+                Spacer()
+                Text(username)
+            }
+            
+            if let fullName = fullName {
+                HStack {
+                    Text("Ime in priimek")
+                    Spacer()
+                    Text(fullName)
+                }
+            }
+        } header: {
+            header(imageName: "person", description: "Ime")
+        }
+    }
+    
+    func section(email: String) -> some View {
+        Section {
+            Text(email)
+        } header: {
+            header(imageName: "envelope", description: "E-naslov")
+        }
+    }
+    
+    func section(favorites: Int) -> some View {
+        Section {
+            HStack {
+                Text("Število")
+                Spacer()
+                Text("\(favorites)")
+            }
+        } header: {
+            header(imageName: "star", description: "Priljubljene besede")
+        }
+    }
+    
+    func logout(action: @escaping () async -> Void) -> some View {
+        Section {
+            AsyncButton(role: .destructive, action: action) {
+                HStack {
+                    Text("Odjava")
+                }
+            }
+        } header: {
+            Image(systemName: "rectangle.portrait.and.arrow.right")
+                .font(.title2)
+                .foregroundColor(.red)
+        }
+    }
+    
+    func header(imageName: String, description: String) -> some View {
+        HStack {
+            Text("\(Image(systemName: imageName))")
+                .font(.title2)
+            
+            Text(description)
+                .font(.body)
+        }
     }
 }
 
